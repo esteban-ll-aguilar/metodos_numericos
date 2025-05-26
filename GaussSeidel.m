b75d04c9ffcf99055b7a72d3ba3c59563cb2f84d
Esteban Leon Aguilar <esteban.aguilar2005@hotmail.com>
2025-05-26 17:51:18 -0500

    gauss sield

---
+++ GaussSeidel.m
@@ -0,0 +1,29 @@
+function [n_interacciones, x_solucion] = GaussSeidel(matriz_ec, v_resultados, tolerancia, max_iteraciones)
+%GAUSSSEIDEL Resuelve un sistema de ecuaciones lineales usando el método de Gauss-Seidel.
+    n_ecuaciones = length(v_resultados);
+    x = zeros(n_ecuaciones, 1);
+
+    for interacciones = 1:max_iteraciones
+        x_antiguo = x;
+        for i = 1:n_ecuaciones
+            lamba = (matriz_ec(i,1:i-1) * x_antiguo(1:i-1) * -1) ...
+                  + (matriz_ec(i,i+1:end) * x_antiguo(i+1:end)*-1);
+            % cambiar los valores de x
+            x(i) = (v_resultados(i) + lamba) / matriz_ec(i,i);
+            %calcular los errores
+            errores = abs(x-x_antiguo);
+        end
+        if all(errores <= tolerancia)
+           break;
+        end
+        if interacciones == max_iteraciones
+            warning('Puede ser que le falto ejecutarse o puede ser que no hay reultado y va hacia el infinito');
+        end
+    end
+    x_solucion = x;
+    n_interacciones = interacciones;
+
+    fprintf('El numero de interacciones es: %d\n', n_interacciones)
+    tabla_resultados = table(x_solucion(:), 'VariableNames', {'Resultados'});
+    disp(tabla_resultados)
+end
--- Main.m
+++ Main.m
@@ -1,10 +1,27 @@
 clc; clear; format long;
 
-funcion = @(x) x.^2;
-derivada = @(x) 2*x;
-a = 0;
-b = 5;
-N = 10;
+% funcion = @(x) x.^2;
+% derivada = @(x) 2*x;
+%------------------------
+% funcion = @(x) tan(x);
+% derivada = @(x) sec(x).^2;
+%------------------------
+% funcion = @(x) 3*x.^2 + 2*x;
+% derivada= @(x) 6*x + 2;
+%------------------------
+% a = 0;
+% b = 5;
+% N = 10;
+% 
+% % DiferenciaNumerica(funcion, a, b, N);
+% DiferenciaConDerivada(funcion, derivada, a, b, N)
+%------------------------
+matriz_ec = [5 2 -3; 2 10 -8; 3 8 13];
+resultados = [1 4 7];
+tolerancia = 0.001;
+max_iteraciones = 5000;
 
-% DiferenciaNumerica(funcion, a, b, N);
-DiferenciaConDerivada(funcion, derivada, a, b, N)
+[n_interacciones, x_solucion] = GaussSeidel(matriz_ec, resultados, tolerancia, max_iteraciones);
+
+%entradas MA, vB, x0, error, max iter
+%salida n_interacciones, ultomos 3 valores
